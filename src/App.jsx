09d71bd70dce053abad197f03b16c8a2d5b6e57f
B1smarQ <bgh050021@gmail.com>
2024-05-23 12:01:19 +0400

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Detail } from './components/Detail';
-import Login from './auth/Login';
 import Navbar from './components/Navbar'
 import Container from './pages/Container'
 import Trending from './pages/Trending';
@@ -13,14 +12,12 @@ import 'react-toastify/dist/ReactToastify.css';
 import Player from './pages/Player';
 import Search from './pages/Search';
 import { Helmet } from "react-helmet";
-import logo from "./assets/images/logo.png"
 
 function App() {
 
   return (
     <MovieProvider>
       <Helmet>
-       <meta property="og:image" content={logo}/>
       </Helmet>
       <ToastContainer
         position="bottom-center"
@@ -41,8 +38,6 @@ function App() {
           <Route path='/' element={<Container />} />
           <Route path='/trending' element={<Trending />} />
           <Route path='/upcoming' element={<Upcoming />} />
-          <Route path='/moviedetail/:id' element={<Detail />} />
-          <Route path="/favorite" element={<Favorite />} />
           <Route path="/search/:query" element={<Container/>}/>
           <Route path="/search/" element={<Container/>}/>
         </Routes>
